f51beed03f99b484837ffb2004733f0c5f979805
Unknown <adhivira@live.com>
2018-06-23 13:25:00 +0200

    asd

---
+++ geolocationtest_HighAccuracy.2.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+    i = 0;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error, options);
+        setTimeout(function () {
+            console.log("STARTME---" + new Date().getTime())
+            for (j = 0; j < 20000; j++) {
+                navigator.geolocation.getCurrentPosition(success, error, options);
+            }
+        }, 15000);
+
+    }
+
+    function success() {
+        i++;
+        if (i == 20000) {
+            console.log("STOPME---" + new Date().getTime())
+        }
+    }
+
+    function error(err) {
+        i++;
+    }
+}
