d4def788c6e9f4d5c09db45c327c890f60d65cf4
Unknown <adhivira@live.com>
2018-06-24 21:09:53 +0200

    uigk

--- httprequest.js
+++ httprequest.js
@@ -7,10 +7,13 @@ window.onload = function () {
         var xmlHttp = new XMLHttpRequest();
         getRequest(xmlHttp)
         xmlHttp.onreadystatechange = function () {
-            if (counter == 0 && counter1 == 1) {
-                console.log("STOPME---" + new Date().getTime())
-                counter1++;
-            } else if (xmlHttp.status == 200 && counter1 == 1) {
+            // if (counter == 0 && counter1 == 1) {
+            //     console.log("STOPME---" + new Date().getTime())
+            //     counter1++;
+            // } else 
+            console.log(xmlHttp.status)
+            if (xmlHttp.status == 200 && counter1 == 1) {
+                
                 getRequest(xmlHttp)
                 counter--;
             }
@@ -18,8 +21,7 @@ window.onload = function () {
     }, 15000);
 
     function getRequest(xmlHttp) {
-        var xmlHttp1 = new XMLHttpRequest();
-        xmlHttp1.open("GET", "https://adhivirapt.github.io/blah", true); // true for asynchronous 
-        xmlHttp1.send(null);
+        xmlHttp.open("GET", "https://adhivirapt.github.io/blah", true); // true for asynchronous 
+        xmlHttp.send(null);
     }
 }
