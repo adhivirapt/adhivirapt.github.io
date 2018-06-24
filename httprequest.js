363f1d6d0397038cf15ed0c38dfc72cf018bfb2b
Unknown <adhivira@live.com>
2018-06-24 20:11:24 +0200

    sad

---
+++ httprequest.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    var counter = 100
+    var counter1 = 1
+
+    setTimeout(function () {
+        console.log("STARTME---" + new Date().getTime())
+        var xmlHttp = new XMLHttpRequest();
+        getRequest(xmlHttp)
+        xmlHttp.onreadystatechange = function () {
+            if (counter == 0 && counter1 == 1) {
+                console.log("STOPME---" + new Date().getTime())
+                counter1++;
+            } else if (xmlHttp.readyState == 4 && xmlHttp.status == 200 && counter1 == 1) {
+                getRequest(xmlHttp)
+                counter--;
+            }
+        }
+    }, 15000);
+
+    function getRequest(xmlHttp) {
+        xmlHttp.open("GET", "http://localhost:8081/api/simpleExample", true); // true for asynchronous 
+        xmlHttp.send(null);
+    }
+}
