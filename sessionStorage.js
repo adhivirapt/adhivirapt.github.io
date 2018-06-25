17a1a59145d81a529ff918a982d63722c42f5f27
Unknown <adhivira@live.com>
2018-06-25 23:05:47 +0200

    awds

--- sessionStorage.js
+++ sessionStorage.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     console.log("STARTME---" + new Date().getTime())
-    for (var i = 0; i < 200000; i++) {
+    for (var i = 0; i < 300000; i++) {
         sessionStorage.setItem(i, "");
         // console.log(localStorage.getItem(i));
     }
